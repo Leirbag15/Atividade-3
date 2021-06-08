724afb6a44e2afb192e83c8d3390b665c99710cd
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-08 17:53:45 -0300

    Create total.c

---
+++ total.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// essa função vai ficar redundante porque C fundamentalmente trabalha com "listas" (array) já sabendo o tamanho máximo delas
+
+int total (int v[]){
+    int t=0
+    for (int i=0; i<n; i++){
+        t++;
+    }
+    return t;
+}
+int n; //size 
+int main()
+{
+    int v[n];
+    for (int i=0; i<n; i++){
+        scanf("%d", &v[i]);
+    }
+}
