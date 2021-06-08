d7da1abcf3f97d87720537ce8d6956bb9cc3c81c
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-08 18:19:38 -0300

    Create divide.c

---
+++ divide.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void divide (int v[], int d){
+    int v1[n];
+    int v2[n];
+    printf("[");
+    for (int i=n-d; i<n; i++){
+        v1[i]=v[i];
+        printf("%d", v1[i]);
+    }
+    printf("]");
+    for (int i=d; i<n; i++){
+        v2[i]=v[i];
+        printf("%d", v2[i]);
+    }
+}
+int n; //size 
+int main()
+{
+    int v[n];
+    for (int i=0; i<n; i++){
+        scanf("%d", &v[i]);
+    }
+}
