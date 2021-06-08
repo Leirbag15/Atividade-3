154b7a16ba06b406a7364c060760e3303e3581e9
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-08 17:39:07 -0300

    Create elemento.c

---
+++ elemento.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int elemento (int e, int v[]){
+    if (e<0){
+        e=e+n;
+    }
+    printf("%d", v[e]);
+}
+int n; //size 
+int main()
+{
+    int v[n];
+    for (int i=0; i<n; i++){
+        scanf("%d", &v[i]);
+    }
+}
