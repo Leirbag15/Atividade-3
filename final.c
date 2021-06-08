e5347a7086fd61e337625b25a9bb2c56e5f3b063
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-08 16:55:11 -0300

    Create final.c

---
+++ final.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int finais(int f, int v[]){
+    for (int i=f; i<n; i++){
+        scanf("%d", &v[i]);
+    }
+}
+int n;
+int main()
+{
+    scanf("%d",&n);
+    int v[n];
+    for (int i=0; i<n; i++){
+        scanf("%d", &v[i]);
+    }
+}
