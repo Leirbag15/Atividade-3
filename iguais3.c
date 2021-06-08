2f3042fafcfbc527f6c115921957ae1de7dde58e
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-08 16:58:24 -0300

    Create iguais3.c

---
+++ iguais3.c
@@ -0,0 +1,11 @@
+int iguais3 (int a, int b, int c){
+    if(a==b && b==c){
+        return 3;
+    }else if (a==b && b!=c){
+        return 2;
+    }else if (a==c && b!=c){
+        return 2;
+    }else if (c==b && a!=c){
+        return 2;
+    }else return 0;
+}
