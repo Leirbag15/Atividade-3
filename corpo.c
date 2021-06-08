0c7e4637fdf9d3adefc6e45ab91c51c0fa7a33a8
Leirbag15 <84280991+Leirbag15@users.noreply.github.com>
2021-06-08 18:03:44 -0300

    Create corpo.c

---
+++ corpo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void corpo (int v[]){
+    if(n<=1){
+        printf(" ");
+        return;
+    }
+    for (int i=0; i<n-1; i++){
+            printf("%d", v[i]);
+    }
+    return;
+}
+int n; //size 
+int main()
+{
+    int v[n];
+    for (int i=0; i<n; i++){
+        scanf("%d", &v[i]);
+    }
+}
